66fc14530bf58ed1f7f3792069cecf45f71ae41b
Lian <3269582028@qq.com>
2021-09-08 08:19:18 +0800

    alexa commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("2021.9.8 commit");
     }
 }
